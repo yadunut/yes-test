52589e8ce1b203ef0263f3b93aea14b75522e5bc
Yadunand Prem <yadunut@gmail.com>
2018-08-27 14:44:55 +0800

    Made python version run faster

--- yes.py
+++ yes.py
@@ -5,7 +5,8 @@ import sys
 expletive = "y"
 if len(sys.argv) != 1:
     expletive = sys.argv[1]
+expletive += "\n"
 while True:
-    print(expletive)
+    sys.stdout.write(expletive)
 
-# [3.84MiB/s]
+# [7.44MiB/s]
