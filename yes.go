186dd6d9359c76d9f474031fd17cca1e2a714429
Yadunand Prem <yadunut@gmail.com>
2018-08-27 14:31:20 +0800

    Update yes.go to imitate `yes`

--- yes.go
+++ yes.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	expletive := "y\n"
+	expletive := "y"
+	if len(os.Args) != 1 {
+		expletive = os.Args[1]
+	}
+	expletive += "\n"
+
 	bb := []byte(expletive)
 
 	for {
 		fmt.Print(bb)
-		// os.Stdout.Write(bb)
 	}
 }
